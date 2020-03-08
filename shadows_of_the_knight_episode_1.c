826251dbb5358c1c850b11e5f1a4c46c7d5eea58
Bill Karvelas <vaskarvelas@gmail.com>
2020-03-08 14:01:42 +0200

    "Some progress"

--- shadows_of_the_knight_episode_1.c
+++ shadows_of_the_knight_episode_1.c
@@ -8,19 +8,22 @@
  * the standard input according to the problem statement.
  **/
 
+void parseDirection(char batman_position[]);
+int binarySearch(int beginning, int end, char direction);
+
 int main()
 {
-    // width of the building.
-    int W;
-    // height of the building.
-    int H;
+    // width, height of the building.
+    int W, H;
     scanf("%d%d", &W, &H);
+
     // maximum number of turns before game over.
     int N;
     scanf("%d", &N);
-    int X0;
-    int Y0;
-    scanf("%d%d", &X0, &Y0);
+
+    // Batman Position
+    int x_bat_pos, y_bat_pos;
+    scanf("%d%d", &x_bat_pos, &y_bat_pos);
 
     // game loop
     while (1)
@@ -33,7 +36,7 @@ int main()
         // To debug: fprintf(stderr, "Debug messages...\n");
         fprintf(stderr, "W: %d | H: %d\n", W, H);
         fprintf(stderr, "Jumps: %d \n", N);
-        fprintf(stderr, "Batman Location (%d,%d) \n", X0, Y0);
+        fprintf(stderr, "Batman Location (%d,%d) \n", x_bat_pos, y_bat_pos);
         fprintf(stderr, "Bomb Location %s \n", bomb_dir);
 
         // the location of the next window Batman should jump to.
@@ -41,4 +44,59 @@ int main()
     }
 
     return 0;
+}
+
+void parseDirection(char batman_position[])
+{
+    int i = 0;
+    while (batman_position[i] != '\0')
+    {
+        // U D L R
+        switch (batman_position[i])
+        {
+            // ama einai Up vres to akrivws Up tetragwno mexri telous
+        case 'U':
+            /* code */
+            break;
+
+            // ama einai Down vres to akrivws Down tetragwno mexri telous
+        case 'D':
+            /* code */
+            break;
+
+            // ama einai Left vres to akrivws Left tetragwno mexri telous
+        case 'L':
+            /* code */
+            break;
+
+            // ama einai Right vres to akrivws Right tetragwno mexri telous
+        case 'R':
+            /* code */
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+int binarySearch(int beginning, int end, char direction)
+{
+    if (end >= beginning)
+    {
+        int mid = beginning + (end - beginning) / 2;
+
+        // If element is smaller than mid, then
+        // it can only be present in left subarray
+        if (direction == 'L' || direction == 'U')
+            return mid - 1;
+
+        // Else the element can only be present
+        // in right subarray
+        return mid + 1;
+    }
+
+    // We reach here when element is not
+    // present in array
+    return -1;
 }
