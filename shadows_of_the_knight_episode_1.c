7340affd11381b37dfd5c8943cc31c9c5f37cd89
Bill Karvelas <vaskarvelas@gmail.com>
2020-03-11 01:22:46 +0200

    Solved

--- shadows_of_the_knight_episode_1.c
+++ shadows_of_the_knight_episode_1.c
@@ -3,18 +3,14 @@
 #include <string.h>
 #include <stdbool.h>
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
-// Data
+// search-rectangle beginning
 struct rect_beginning
 {
     int x;
     int y;
 } rect_beg;
 
+// search-rectangle ending
 struct rect_ending
 {
     int x;
@@ -27,6 +23,7 @@ struct general_building
     int H;
 } building;
 
+// current batman position
 struct batman_position
 {
     int x;
@@ -39,15 +36,14 @@ struct previews_batman_position
     int y;
 } prev_bat_pos;
 
-// N = maximum number of turns before game over.
+// is the beginning of the game?
 bool start_of_game = true;
 
 void parse_direction(char bomb_direction[], bool start_of_game, struct rect_beginning *rect_b, struct rect_ending *rect_e);
-int binary_search_2d(int beginning, int end, char direction);
+int binary_search_2d(int beginning, int end);
 
 int main()
 {
-
     // width, height of the building.
     scanf("%d%d", &building.W, &building.H);
 
@@ -71,16 +67,17 @@ int main()
 
         parse_direction(bomb_dir, start_of_game, &rect_beg, &rect_end);
 
-        // Write an action using printf(). DON'T FORGET THE TRAILING \n
-        // To debug: fprintf(stderr, "Debug messages...\n");
-        fprintf(stderr, "W: %d | H: %d\n", building.W, building.H);
-        fprintf(stderr, "Jumps: %d \n", N);
-        fprintf(stderr, "Batman Location (%d,%d) \n", bat_pos.x, bat_pos.y);
-        fprintf(stderr, "Bomb Location %s \n", bomb_dir);
+        bat_pos.x = binary_search_2d(rect_beg.x, rect_end.x);
+        bat_pos.y = binary_search_2d(rect_beg.y, rect_end.y);
+
+        printf("%d %d\n", bat_pos.x, bat_pos.y);
 
         // the location of the next window Batman should jump to.
-        // printf("3 7\n");
-        start_of_game = false;
+        prev_bat_pos.x = bat_pos.x;
+        prev_bat_pos.y = bat_pos.y;
+
+        if (start_of_game == true)
+            start_of_game = false;
     }
 
     return 0;
@@ -92,15 +89,12 @@ void parse_direction(char bomb_direction[], bool start_of_game, struct rect_begi
     int i = 0;
     while (bomb_direction[i] != '\0')
     {
-        // Initial batman position, direction
-
-        // U D L R
-        switch (bomb_direction[i])
+        // direction
+        switch (bomb_direction[i++])
         {
-            // ama einai Up vres to akrivws Up tetragwno mexri telous
         case 'U':
             // if it is the first move
-            if (start_of_game)
+            if (start_of_game == true)
             {
                 // search-rectangle beginning
                 rect_b->y = 0;
@@ -115,13 +109,12 @@ void parse_direction(char bomb_direction[], bool start_of_game, struct rect_begi
             rect_e->y = bat_pos.y - 1;
             break;
 
-            // ama einai Down vres to akrivws Down tetragwno mexri telous
         case 'D':
             // search-rectangle beginning
             rect_b->y = bat_pos.y + 1;
 
             // if it is the first move
-            if (start_of_game)
+            if (start_of_game == true)
             {
                 // search-rectangle end
                 rect_e->y = building.H - 1;
@@ -133,10 +126,9 @@ void parse_direction(char bomb_direction[], bool start_of_game, struct rect_begi
             }
             break;
 
-            // ama einai Left vres to akrivws Left tetragwno mexri telous
         case 'L':
             // if it is the first move
-            if (start_of_game)
+            if (start_of_game == true)
             {
                 // search-rectangle beginning
                 rect_b->x = 0;
@@ -151,13 +143,12 @@ void parse_direction(char bomb_direction[], bool start_of_game, struct rect_begi
             rect_e->x = bat_pos.x - 1;
             break;
 
-            // ama einai Right vres to akrivws Right tetragwno mexri telous
         case 'R':
             // search-rectangle beginning
             rect_b->x = bat_pos.x + 1;
 
             // if it is the first move
-            if (start_of_game)
+            if (start_of_game == true)
             {
                 // search-rectangle end
                 rect_e->x = building.W - 1;
@@ -175,23 +166,7 @@ void parse_direction(char bomb_direction[], bool start_of_game, struct rect_begi
     }
 }
 
-int binary_search_2d(int beginning, int end, char direction)
+int binary_search_2d(int beginning, int end)
 {
-    if (end >= beginning)
-    {
-        int mid = beginning + (end - beginning) / 2;
-
-        // If element is smaller than mid, then
-        // it can only be present in left subarray
-        if (direction == 'L' || direction == 'U')
-            return mid - 1;
-
-        // Else the element can only be present
-        // in right subarray
-        return mid + 1;
-    }
-
-    // We reach here when element is not
-    // present in array
-    return -1;
+    return (beginning + end) / 2;
 }
